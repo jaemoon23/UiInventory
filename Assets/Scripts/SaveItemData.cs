451f432f647074b8cdc075ff1def42c46d2be0dc
이재문 <990914s@gmail.com>
2025-09-04 17:21:49 +0900

    UI 인벤토리 생성

---
+++ Assets/Scripts/SaveItemData.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System;
+using Newtonsoft.Json;
+
+[Serializable]
+public class SaveItemData
+{
+    public Guid instanceId;
+
+    [JsonConverter(typeof(ItemDataConverter))]
+    public ItemData itemData;
+    public DateTime creationTime;
+
+    public SaveItemData()
+    {
+        instanceId = Guid.NewGuid();
+        creationTime = DateTime.Now;
+    }
+
+}
